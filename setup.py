2174ce328292a2b4a6b6ce0f8b42f257d3c65d2e
unknown <novakkaz6@gmail.com>
2024-03-12 00:57:50 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+
+def get_requirments(file_path:str,) -> List[str]:
+    '''
+    this function will return a list of requirments
+    '''
+    reqirments = []
+    with open(file_path) as file_obj:
+        requirment = file_obj.readlines()
+        requirment = [req.replace('\n','') for req in requirment]
+        if '-e .' in requirment:
+            requirment.remove('-e .')
+
+
+setup (
+    name = 'mlproject',
+    version = '0.0.1',
+    author = 'Saksham',
+    author_email = 'sakshamkale46@gmail.com',
+    packages = find_packages(),
+    install_requires = ['pandas','numpy','seaborn'],
+    install_requirments = get_requirments('requirments.txt')
+)
