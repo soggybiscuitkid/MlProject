619f0356172fc777622cc001f1b188b79a95098d
unknown <novakkaz6@gmail.com>
2024-03-12 23:57:38 +0530

    logger.py

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,6 @@
 import sys 
+import logging
+
 
 def error_message_details(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -8,9 +10,16 @@ def error_message_details(error,error_detail:sys):
     return error_message
 
 class CustomException(Exception):
-    def __init__(self, error_message,error_detias):
-        super.__init__(error_message)
+    def __init__(self, error_message,error_detias:sys):
+        super().__init__(error_message)
         self.error_message = error_message_details(error_message,error_detail=error_detias)
 
     def __str__(self) -> str:
         return self.error_message
+    
+if __name__ == "__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info('0 error')
+        raise CustomException(e,sys)
